f3616d8227eb08a5407466cb971adbf7ed6d50a5
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-26 20:15:28 +0300

    Initial commit

---
+++ exercise02.py
@@ -0,0 +1,13 @@
+person = {
+    "name": "Nikolaos",
+    "surname": "Zisos",
+    "father_name": "Antonios",
+    "birth_date": "21/3/1972",
+    "address": "Limnou 7",
+    "phone": "6942553316"
+    }
+print("Ονοματεπώνυμο: " + person["name"] + " " + person["father_name"] + " " + person["surname"])
+print("Ημερομηνία γέννησης: " + person["birth_date"])
+print("Διεύθυνση: " + person["address"])
+print("Τηλέφωνο: " + person["phone"])
+
