e6f7f5e01bd42f5de33d7770f0f00c75f1f678c2
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-27 19:54:03 +0300

    Initial commit

--- dict.loop.py
+++ dict.loop.py
@@ -7,7 +7,14 @@ heroes_weapons = {
     "Venom": "Web-Shooters"
 }
 
-print("Key-Value Loop:")
+print ("Key-Value Loop:")
 for key, value in heroes_weapons.items():
     print (key + " has " + value)
 
+print ("\nOrdered Key Loop:")
+for key in sorted(heroes_weapons.keys()):
+    print (key + " has " + heroes_weapons[key])
+
+print ("\nWeapons Gallery:")
+for value in set(heroes_weapons.values()):
+    print (value, end="")
