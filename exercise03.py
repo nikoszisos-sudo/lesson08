30e36d823effb89f26953ab519b75aa0c9dfe29a
Nikolaos Zisos <nikoszisos@gmail.com>
2026-05-31 09:10:58 +0300

    Initial commit

--- exercise03.py
+++ exercise03.py
@@ -5,7 +5,7 @@ for i in range(1, 6+1):
     numbers[i] = 0
 print(numbers)
 
-for i in range(1, 30+1):
+for i in range(1, 1000000+1):
     num = randrange(1, 6+1)
     numbers[num] += 1
 print(numbers)
