785df8f1bc11926246b4e222f34d322b71f46dc6
Nikolaos Zisos <nikoszisos@gmail.com>
2026-06-06 17:50:32 +0300

    Initial commit

--- exercise03.py
+++ exercise03.py
@@ -1,11 +1,20 @@
 from random import randrange
-
+N=10
 numbers = {}
 for i in range(1, 6+1):
     numbers[i] = 0
 print(numbers)
 
-for i in range(1, 1000000+1):
+for i in range(1, N+1):
+    num = randrange(1, 6+1)
+    numbers[num] += 1
+print(numbers)
+
+for i in numbers:
+    percent = ((numbers[i]/N)*100)
+    print ("Το νούμερο " + str(i) + " έχει ποσοστό " + str(percent) + "%")
+
+for i in range(1, N+1):
     num = randrange(1, 6+1)
     numbers[num] += 1
 print(numbers)
